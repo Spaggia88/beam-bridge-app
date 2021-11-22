132cec1e6934d027050c4489417bba51a17c392e
Alex Starun <alex.starun@beam-mw.com>
2021-11-22 19:34:23 +0100

    Fixed decimals

--- src/app/core/types.tsx
+++ src/app/core/types.tsx
@@ -21,7 +21,7 @@ export interface Transaction {
 }
 
 export const currencies : Currency[] = [
-    {name: "bUSDT", id: 1, decimals: 8, cid: '7617af66e36084a763019544094bf1586096b2befef348c14d369c69aa9c99f7'},
-    {name:'bWBTC', id: 2, decimals: 8, cid: '7617af66e36084a763019544094bf1586096b2befef348c14d369c69aa9c99f7'},
-    {name:'bDAI', id: 3, decimals: 8, cid: '7617af66e36084a763019544094bf1586096b2befef348c14d369c69aa9c99f7'}
+    {name: "bUSDT", id: 1, decimals: 6, cid: '2acc70ebd1bdda9d58c543e83560a35f5f9b06a1861ebde83500790c790d92fe'},
+    {name:'bETH', id: 2, decimals: 8, cid: '40306461ecccb777b308643bd6499f9e22c7c6373edb11aae1d32f112c4a79ac'},
+    {name:'bDAI', id: 3, decimals: 8, cid: '8d16dab3c94df2edc7e946daadb7df406f920ee0447bedc8744c57a89c1f1fbd'}
 ];
